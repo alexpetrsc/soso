79b9158fc8fb83af2a28f13da2b7f8292e000a61
alexpetrsc <alexpetrsc@proton.me>
2026-07-22 18:12:15 +0300

    Create HttpStatus.h

---
+++ HttpStatus.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <string>
+
+struct HttpStatus
+{
+    int code;
+
+    std::string title;
+
+    std::string category;
+
+    std::string description;
+
+    std::string example;
+};
